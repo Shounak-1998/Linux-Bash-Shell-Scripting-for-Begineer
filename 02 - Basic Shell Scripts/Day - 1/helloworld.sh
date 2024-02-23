36bfda710e4d2b178eb0ea9b1c06963cc6bde665
Shounak Khulape <69784641+Shounak-1998@users.noreply.github.com>
2024-02-23 13:32:45 +0530

    Add files via upload
    This is basic hello world shell file

---
+++ 02 - Basic Shell Scripts/Day - 1/helloworld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author: Shounak Khulape
+# Date Created: 02/23/2024
+# Description:	This script will output "Hello World" on the screen
+# Date Modified: 02/23/2024
+echo
+echo Hello World
+echo
