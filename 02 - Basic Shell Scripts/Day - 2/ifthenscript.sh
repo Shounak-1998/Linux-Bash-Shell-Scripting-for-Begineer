0c7f60efa58078197b5c2b892014ebf5b52bb1eb
Shounak Khulape <69784641+Shounak-1998@users.noreply.github.com>
2024-02-23 15:19:06 +0530

    Add files via upload

---
+++ 02 - Basic Shell Scripts/Day - 2/ifthenscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Date Created: 02/23/2024
+# Author: Shounak Khulape
+# Description: This script is regarding if-then command
+# Date Modified: 02/23/2024
+
+count=100
+if [ $count -eq 100 ]
+then
+	echo Count is 100
+else
+	echo Count is not 100
+fi
