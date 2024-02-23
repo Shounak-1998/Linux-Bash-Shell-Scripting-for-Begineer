8235767751d5000225cc66687546bf1665b95d7f
Shounak Khulape <69784641+Shounak-1998@users.noreply.github.com>
2024-02-23 13:49:46 +0530

    Add files via upload
    This file is shell which perform basic shell tasks

---
+++ 02 - Basic Shell Scripts/Day - 2/basictasks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: Shounak Khulape
+# Date Created:	02/23/2024
+# Description: Run Basic linux tasks
+# Date Modified: 02/23/2024
+
+echo
+echo This script will run a few basic tasks
+echo
+pwd
+ls
+whoami
+date
+cal
+touch a b c
