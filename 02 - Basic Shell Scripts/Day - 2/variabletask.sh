ca76fa376e7f7611b0fbafe31bd537bba4e41ea8
Shounak Khulape <69784641+Shounak-1998@users.noreply.github.com>
2024-02-23 14:13:32 +0530

    Add files via upload
    This is shell created for declaring variables

---
+++ 02 - Basic Shell Scripts/Day - 2/variabletask.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Author: Shounak Khulape
+# Date Created: 02/23/2024
+# Description: This script is screated for declaring varaiable command
+# Date Modified: 02/23/2024
+echo
+# Define Variables
+p=pwd
+l=ls
+wi=whoami
+d=date
+c="cal 2018"
+
+echo Run variables tasks
+echo
+$p
+$l
+$wi
+$d
+$c
