c9fe9f9aa774c4eb407aab944f71c9ae0c0cff3e
Shounak Khulape <69784641+Shounak-1998@users.noreply.github.com>
2024-02-23 13:58:16 +0530

    Add files via upload
    This shell is created for basic admin tasks

---
+++ 02 - Basic Shell Scripts/Day - 2/admintasks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: Shounak Khulape
+# Date Created: 02/23/2023
+# Description: This script will run basic admin command
+# Date Modified: 02/23/2024
+echo
+echo This script will run basic administrative commands
+echo
+top
+echo
+df -h
+echo
+uptime
+echo
+iostat
+echo
+echo End of Script
