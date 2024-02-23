7c1bf7ca1414f3632a12011ffebcf52dac9e1527
Shounak Khulape <69784641+Shounak-1998@users.noreply.github.com>
2024-02-23 14:27:40 +0530

    Add files via upload
    This is shell which will take input from user in shell

---
+++ 02 - Basic Shell Scripts/Day - 2/inputscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: Shounak Khulape
+# Date Created: 02/23/2023
+# Description: This script is created to dispaly input/output command.
+# Date Modified: 02/23/2024
+a=`hostname`
+echo
+echo Hello, my server name is $a
+echo
+echo Hello, My Name is Shounak Khulape
+echo
+echo What is your name?
+read namecont
+echo
+echo Hello $namecont
+echo
+
