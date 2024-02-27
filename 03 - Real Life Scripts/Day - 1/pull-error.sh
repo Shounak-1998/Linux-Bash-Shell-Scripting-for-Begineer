9fc0dc8e221a926d510b5964e6a308b3dcd09a6b
Shounak Khulape <69784641+Shounak-1998@users.noreply.github.com>
2024-02-27 16:22:19 +0530

    Add files via upload

---
+++ 03 - Real Life Scripts/Day - 1/pull-error.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Date Created: 02/26/2024
+# Author: Shounak Khulape
+# Description: This script is to check other if statement.
+# Date Modified: 02/26/2024
+
+grep -i error /home/oracle/scripts/varlogerror/messages > /home/oracle/scripts/varlogerror/push-error
